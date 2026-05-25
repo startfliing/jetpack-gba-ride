ff67fcddb0afecb5d4b7bfb505879196197b893e
startfliing <jbhelgas@gmail.com>
2026-05-25 01:20:04 -0700

    update struct variable names

--- include/haz.hpp
+++ include/haz.hpp
@@ -7,8 +7,8 @@
 
 struct hazardAsset{
     const unsigned short* map;
-    u8 tileWidth, tileHeight;
-    u16 tileX, tileY;
+    u8 width, height;
+    u16 x, y;
 };
 
 extern hazardAsset diag;
@@ -40,52 +40,53 @@ class YellowLaser : public Hazard {
     public:
         YellowLaser(hazardAsset ha){
             asset = ha;
-            hitbox = new Pill((ha.tileX * 8) + 8, (ha.tileY * 8) + 8, ((ha.tileX + ha.tileWidth) * 8) - 8, ((ha.tileY + ha.tileHeight) * 8) - 8, 4);
+            hitbox = new Pill((ha.x * 8) + 8, (ha.y * 8) + 8, ((ha.x + ha.width) * 8) - 8, ((ha.y + ha.height) * 8) - 8, 4);
             rendered = false;
+            erased = false;
         };
 
         void update(int scrollX){
             //>> 4 for FIXED notation >> 3 for pixel-to-tile ratio
-            bool shouldRender = rendered ? false : (scrollX>>7)+32 > asset.tileX;
+            bool shouldRender = rendered ? false : (scrollX>>7)+32 > asset.x;
             if(shouldRender) render();
 
-            bool shouldErase = erased ? false : (scrollX>>7) > asset.tileX + (asset.tileWidth + 1);
+            bool shouldErase = erased ? false : (scrollX>>7) > asset.x + (asset.width + 1);
             if(shouldErase) erase();
         }
 
         void render(){
             rendered = true;
             // tile between 0 - 63 for drawing to BG
-            u8 bgTileX = asset.tileX & 0x3F;
-            for(int i = 0; i < asset.tileHeight; i++){
-                //use tileX to decide which sbb
-                int rowStartInd = 32*(asset.tileY + i) + /*start x*/(asset.tileX & 0x1F);
+            u8 bgx = asset.x & 0x3F;
+            for(int i = 0; i < asset.height; i++){
+                //use x to decide which sbb
+                int rowStartInd = 32*(asset.y + i) + /*start x*/(asset.x & 0x1F);
                 
-                if(bgTileX < (31 - asset.tileWidth)){
-                    memcpy16(&se_mem[18][rowStartInd], &asset.map[asset.tileWidth * i], asset.tileWidth);
-                } else if(bgTileX < 31){
+                if(bgx < (31 - asset.width)){
+                    memcpy16(&se_mem[18][rowStartInd], &asset.map[asset.width * i], asset.width);
+                } else if(bgx < 31){
                     memcpy16(
                         &se_mem[18][rowStartInd], 
-                        &asset.map[asset.tileWidth * i], 
-                        32 - bgTileX
+                        &asset.map[asset.width * i], 
+                        32 - bgx
                     );
 
-                    memcpy16(&se_mem[19][rowStartInd + (32 - bgTileX)], 
-                        &asset.map[(asset.tileWidth * i) + (31 - bgTileX)], 
-                        asset.tileWidth - (31 - bgTileX)
+                    memcpy16(&se_mem[19][rowStartInd + (32 - bgx)], 
+                        &asset.map[(asset.width * i) + (31 - bgx)], 
+                        asset.width - (31 - bgx)
                     );
-                } else if(bgTileX < (63 - asset.tileWidth)){ // fully in sbb 19
-                    memcpy16(&se_mem[19][rowStartInd], &asset.map[asset.tileWidth * i], asset.tileWidth);
+                } else if(bgx < (63 - asset.width)){ // fully in sbb 19
+                    memcpy16(&se_mem[19][rowStartInd], &asset.map[asset.width * i], asset.width);
                 } else {
                     memcpy16(
                         &se_mem[19][rowStartInd], 
-                        &asset.map[asset.tileWidth * i], 
-                        64 - bgTileX
+                        &asset.map[asset.width * i], 
+                        64 - bgx
                     );
 
-                    memcpy16(&se_mem[18][rowStartInd + (64 - bgTileX)], 
-                        &asset.map[(asset.tileWidth * i) + (63 - bgTileX)], 
-                        asset.tileWidth - (63 - bgTileX)
+                    memcpy16(&se_mem[18][rowStartInd + (64 - bgx)], 
+                        &asset.map[(asset.width * i) + (63 - bgx)], 
+                        asset.width - (63 - bgx)
                     );
                 }
             }
@@ -95,37 +96,37 @@ class YellowLaser : public Hazard {
             Terminal::log("erasing!");
             erased = true;
             // tile between 0 - 63 for drawing to BG
-            u8 bgTileX = asset.tileX & 0x3F;
-            for(int i = 0; i < asset.tileHeight; i++){
-                //use tileX to decide which sbb
-                int rowStartInd = 32*(asset.tileY + i) + /*start x*/(asset.tileX & 0x1F);
+            u8 bgx = asset.x & 0x3F;
+            for(int i = 0; i < asset.height; i++){
+                //use x to decide which sbb
+                int rowStartInd = 32*(asset.y + i) + /*start x*/(asset.x & 0x1F);
                 
-                if(bgTileX < (31 - asset.tileWidth)){
-                    memset16(&se_mem[18][rowStartInd], 0, asset.tileWidth);
-                } else if(bgTileX < 31){
+                if(bgx < (31 - asset.width)){
+                    memset16(&se_mem[18][rowStartInd], 0, asset.width);
+                } else if(bgx < 31){
                     memset16(
                         &se_mem[18][rowStartInd], 
                         0, 
-                        31 - bgTileX
+                        31 - bgx
                     );
 
-                    memset16(&se_mem[19][rowStartInd + (32 - bgTileX)], 
+                    memset16(&se_mem[19][rowStartInd + (32 - bgx)], 
                         0, 
-                        asset.tileWidth - (31 - bgTileX)
+                        asset.width - (31 - bgx)
                     );
 
-                } else if(bgTileX < (63 - asset.tileWidth)){ // fully in sbb 19
-                    memset16(&se_mem[19][rowStartInd], 0, asset.tileWidth);
+                } else if(bgx < (63 - asset.width)){ // fully in sbb 19
+                    memset16(&se_mem[19][rowStartInd], 0, asset.width);
                 } else {
                     memset16(
                         &se_mem[19][rowStartInd], 
                         0, 
-                        64 - bgTileX
+                        64 - bgx
                     );
 
-                    memset16(&se_mem[18][rowStartInd + (64 - bgTileX)], 
+                    memset16(&se_mem[18][rowStartInd + (64 - bgx)], 
                         0, 
-                        asset.tileWidth - (63 - bgTileX)
+                        asset.width - (63 - bgx)
                     );
                 }
             }
