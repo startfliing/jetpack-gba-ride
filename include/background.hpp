6e911722c3c796af0b7685dacd8f53bf69de7e84
startfliing <jbhelgas@gmail.com>
2026-06-06 15:37:34 -0700

    refactor backgroundEntry selection

--- include/background.hpp
+++ include/background.hpp
@@ -10,48 +10,20 @@ struct backgroundEntry{
     const SCREENBLOCK* map;
 };
 
-extern backgroundEntry entries[];
-
 class BackgroundManager{
     public:
-        BackgroundManager(u16 sb, u16 cb){
-            current_screenblock = -1;
-            sbb = sb;
-            cbb = cb;
-            be = entries[3];
-            current_background_duration = 1;
-        };
+        BackgroundManager(u16 sb, u16 cb);
 
-        void init(){
-            LZ77UnCompVram(tilesetPal, pal_bg_mem);
-            LZ77UnCompVram(tilesetTiles, tile_mem[cbb]);
-            memcpy16(&se_mem[sbb], be.map[0], sizeof(SCREENBLOCK)/2);
-        };
-
-        void update(int scrollX){ 
-            //find the sb 16 pixels behind the player a.k.a the one the player is in
-            int playerSB = (scrollX - (16<<4))>>12; //>>4 to get pixels >>3 more to get tiles >>5 to get whole screen Blocks
-            
-            if(playerSB != current_screenblock){ //transition time
-
-                if(current_background_duration == be.duration){
-                    be = entries[playerSB & 1]; //entry 0 or 1
-                    current_background_duration = 0;
-                }
-                // sbb + 1 - (playerSB & 1)
-                // 
-                memcpy16(se_mem[sbb + 1 - (playerSB & 1)], be.map[current_background_duration], sizeof(SCREENBLOCK)/2);
-                //Time to Select a new background
-                current_background_duration++;
-                current_screenblock = playerSB;
-            }
-
-        };
+        void init();
+        void update(int scrollX);
 
     private:
+        void selectNewBE();
+
         int current_screenblock;
         u16 sbb, cbb;
 
+        u16 totalBECt;
         backgroundEntry be;
         u16 current_background_duration; //how long we've been on this background
 };
--- source/background.cpp
+++ source/background.cpp
@@ -5,29 +5,50 @@
 #include "zone3.h"
 #include "startZone.h"
 
-backgroundEntry zone1 = {
-    1,
-    (SCREENBLOCK*)zone1Map
-};
-
-backgroundEntry zone2 = {
-    1,
-    (SCREENBLOCK*)zone2Map
-};
-
-backgroundEntry zone3 = {
-    3,
-    (SCREENBLOCK*)zone3Map
-};
-
-backgroundEntry startZone = {
-    2,
-    (SCREENBLOCK*)startZoneMap
-};
-
 backgroundEntry entries[] = {
-    zone1,
-    zone2,
-    zone3,
-    startZone
+    { 2, (SCREENBLOCK*)startZoneMap },
+    { 1, (SCREENBLOCK*)zone1Map },
+    { 1, (SCREENBLOCK*)zone2Map },
+    { 3, (SCREENBLOCK*)zone3Map }
 };
+
+BackgroundManager::BackgroundManager(u16 sb, u16 cb){
+    current_screenblock = -1;
+    sbb = sb;
+    cbb = cb;
+    be = entries[0];
+    current_background_duration = 1;
+    totalBECt = sizeof(entries) / sizeof(backgroundEntry);
+};
+
+void BackgroundManager::init(){
+    LZ77UnCompVram(tilesetPal, pal_bg_mem);
+    LZ77UnCompVram(tilesetTiles, tile_mem[cbb]);
+    memcpy16(&se_mem[sbb], be.map[0], sizeof(SCREENBLOCK)/2);
+};
+
+void BackgroundManager::selectNewBE(){
+    // totalBECt, except 0
+    int random = (qran() % (totalBECt-1))+1;
+    be = entries[random];
+    current_background_duration = 0;
+}
+
+void BackgroundManager::update(int scrollX){ 
+    //find the sb 16 pixels behind the player a.k.a the one the player is in
+    int playerSB = (scrollX - (16<<4))>>12; //>>4 to get pixels >>3 more to get tiles >>5 to get whole screen Blocks
+    
+    if(playerSB != current_screenblock){ //transition time
+
+        if(current_background_duration == be.duration){
+            selectNewBE();
+        }
+        // sbb + 1 - (playerSB & 1)
+        // if you are in 0, update 1, if in 1 update 0
+        memcpy16(se_mem[sbb + 1 - (playerSB & 1)], be.map[current_background_duration], sizeof(SCREENBLOCK)/2);
+        //Time to Select a new background
+        current_background_duration++;
+        current_screenblock = playerSB;
+    }
+
+}
